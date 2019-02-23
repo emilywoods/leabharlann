5006a996dabb0fe2e4e10f2dc2fad77c5cf22031
Emily Woods <emily@crate.io>
2019-02-23 12:30:19 +0100

    Decompose steps into separate functions. Append to file instead of overwriting it
    Format main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,78 @@
 use std::env;
-use std::fs::File;
-use std::io::{stdin,stdout,Write};
+use std::fs::OpenOptions;
+use std::io::{stdin, stdout, Write};
 
-fn main() -> std::io::Result<()> {
+fn main() {
     let args: Vec<String> = env::args().collect();
-    let query = &args[1];
+    let _query = parse_config(&args);
 
-
-    let mut book_title = String::new();
-    let mut author = String::new();
-    let mut start_date = String::new();
-    let mut end_date= String::new();
-
-    let _=stdout().flush(); // what is the purpose of this?
-
-    println!("What are you reading?");
-    stdin().read_line(&mut book_title).expect("Please enter a book title");
-
-    println!("Who wrote it?");
-    stdin().read_line(&mut author).expect("Please enter the author(s)");
-
-    println!("When did you start?");
-    stdin().read_line(&mut start_date).expect("Please enter time you started");
-
-    println!("When did you finish?");
-    stdin().read_line(&mut end_date).expect("No end date entered. The book is marked as 'in-progress'");
+    let (book_title, author, start_date, end_date) = user_interaction();
 
     let book_title = book_title.trim();
     let author = author.trim();
     let start_date = start_date.trim();
-    let end_date= end_date.trim();
+    let end_date = end_date.trim();
 
-    println!("Reading {:?}, by {:?}. Started: {:?} and ended: {:?}", book_title.trim(), author.trim(), start_date.trim(), end_date.trim());
+    println!(
+        "Reading {:?}, by {:?}. Started: {:?} and ended: {:?}",
+        book_title, author, start_date, end_date
+    );
 
-    let mut file = File::create("reading.txt")?;
+    write_to_file(book_title, author, start_date, end_date).expect("error writing to file");
+}
 
-    file.write_fmt(format_args!("Reading {:?}, by {:?}. Started: {:?} and ended: {:?}", book_title, author, start_date, end_date))?;
+fn parse_config(args: &[String]) -> (&str) {
+    let query = &args[1];
+    query
+}
+
+fn user_interaction() -> (String, String, String, String) {
+    let mut book_title = String::new();
+    let mut author = String::new();
+    let mut start_date = String::new();
+    let mut end_date = String::new();
+
+    let _ = stdout().flush(); // what is the purpose of this?
+
+    println!("What are you reading?");
+    stdin()
+        .read_line(&mut book_title)
+        .expect("Please enter a book title");
+
+    println!("Who wrote it?");
+    stdin()
+        .read_line(&mut author)
+        .expect("Please enter the author(s)");
+
+    println!("When did you start?");
+    stdin()
+        .read_line(&mut start_date)
+        .expect("Please enter time you started");
+
+    println!("When did you finish?");
+    stdin()
+        .read_line(&mut end_date)
+        .expect("No end date entered. The book is marked as 'in-progress'");
+
+    (book_title, author, start_date, end_date)
+}
+
+fn write_to_file(
+    book_title: &str,
+    author: &str,
+    start_date: &str,
+    end_date: &str,
+) -> std::io::Result<()> {
+    let mut file = OpenOptions::new()
+        .read(true)
+        .append(true)
+        .create(true)
+        .open("reading.txt")?;
+
+    file.write_fmt(format_args!(
+        "Reading {:?}, by {:?}. Started: {:?} and ended: {:?}\n",
+        book_title, author, start_date, end_date
+    ))?;
 
     Ok(())
 }
